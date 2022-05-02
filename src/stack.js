6f5454eb5d17f45527475d6fba3b3dec9f0aedd7
Eva Kerrigan <evakerrigan@gmail.com>
2022-05-03 02:13:34 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ let stackList = [];
+
+
+class Stack {  
+
+  push(element) {    
+    return stackList.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return stackList.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return stackList.at(-1);
   }
 }
 
